5bce6f348f572f80850b1a6d8598001ee501bf77
mhn-ii <nimrim@mcmaster.ca>
2019-05-31 10:13:47 -0400

    Revert "restart"
    This reverts commit e83f7edcb86d2cabad9214b27c3634dab9aff180.

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.send({ response: "I am alive" }).status(200);
+});
+
+router.get("/home", (req, res) => {
+  res.send({ response: "mos zoubi" }).status(200);
+});
+
+module.exports = router;
